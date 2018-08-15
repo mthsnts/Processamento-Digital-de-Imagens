cb03900b0117cc3f027c2efa95be82e6b3ec23a0
Matheus <34779272+mthsnts@users.noreply.github.com>
2018-08-14 21:11:31 -0300

    Commit aula 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,50 @@
 package sample;
 
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
 public class Controller {
+
+	@FXML private Button image1_button;
+	@FXML private ImageView image_view_1;
+	@FXML private Button image2_button;
+	@FXML private ImageView image_view_2;
+	@FXML private Button save_button;
+	@FXML private ImageView image_view_3;
+    @FXML private Label label_R;
+    @FXML private Label label_G;
+    @FXML private Label label_B;
+	
+    private File f;
+    
+	private File openImage1(){
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter
+				("Imagens", "*.jpg", "*JPG", "**.png", "*PNG", "*.gif", "*.GIF", "*.bmp", "*.BMP"));
+		File img = fileChooser.showOpenDialog(null);
+		try{
+			if(img != null){
+				return img;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@FXML 
+	public void openImage2(){
+		
+	}
+	
+	@FXML
+	public void save(){
+		
+	}
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,12 +12,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1366, 700));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+    
 }
