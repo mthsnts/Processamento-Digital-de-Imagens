6d11fb261e9eec10f9bfd60fd20ca22dbce1ed95
Matheus <34779272+mthsnts@users.noreply.github.com>
2018-10-30 22:04:05 -0200

    Erosão e dilatação funcionando

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -42,142 +42,158 @@ import utils.OpenCVUtils;
 
 public class Controller {
 
-	@FXML private ImageView image_view_1;
-	@FXML private ImageView image_view_2;
-	@FXML private ImageView image_view_3;
+	@FXML
+	private ImageView image_view_1;
+	@FXML
+	private ImageView image_view_2;
+	@FXML
+	private ImageView image_view_3;
 
-    @FXML private Label label_R;
-    @FXML private Label label_G;
-    @FXML private Label label_B;
+	@FXML
+	private Label label_R;
+	@FXML
+	private Label label_G;
+	@FXML
+	private Label label_B;
 
-    @FXML private TextField r_percentage;
-    @FXML private TextField g_percentage;
-    @FXML private TextField b_percentage;
-    
-    @FXML private RadioButton rX;
-    @FXML private RadioButton rC;
-    @FXML private RadioButton r3;
+	@FXML
+	private TextField r_percentage;
+	@FXML
+	private TextField g_percentage;
+	@FXML
+	private TextField b_percentage;
 
-    @FXML private Slider slider;
-    
-    @FXML private TextField pctImg1;
-    @FXML private TextField pctImg2;
-    
-    @FXML
-    private TextField krnlsz;
+	@FXML
+	private RadioButton rX;
+	@FXML
+	private RadioButton rC;
+	@FXML
+	private RadioButton r3;
 
-    @FXML
-    private TextField tp;
-	
-    private File f;
-    private Image img_1, img_2, img_3;
-    private OpenCVUtils converter;
-    private Mat matImgSrc;
-    private Mat matImgDst = new Mat();
-    
-	private File selectImage(){
+	@FXML
+	private Slider slider;
+
+	@FXML
+	private TextField pctImg1;
+	@FXML
+	private TextField pctImg2;
+
+	@FXML
+	private TextField krnlsz;
+
+	@FXML
+	private TextField tp;
+
+	private File f;
+	private Image img_1, img_2, img_3;
+	private OpenCVUtils converter;
+	private Mat matImgSrc;
+
+	private File selectImage() {
 		FileChooser fileChooser = new FileChooser();
 
-		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter
-				("Imagens", "*.jpg", "*JPG", "**.png", "*PNG", "*.gif", "*.GIF", "*.bmp", "*.BMP"));
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Imagens", "*.jpg", "*JPG", "**.png",
+				"*PNG", "*.gif", "*.GIF", "*.bmp", "*.BMP"));
 		File img = fileChooser.showOpenDialog(null);
-		try{
-			if(img != null){
+		try {
+			if (img != null) {
 				return img;
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-//	@FXML
-//	public void detectaFaces(){
-//		f = selectImage();
-//		String path = f.getAbsolutePath();
-//		
-//		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//
-//		CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_alt.xml");
-//		Mat image = Imgcodecs.imread("rostos.jpg");
-//
-//		MatOfRect faceDetections = new MatOfRect();
-//		faceDetector.detectMultiScale(image, faceDetections);
-//
-//		System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
-//
-//		for (Rect rect : faceDetections.toArray()) {
-//			Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
-//					new Scalar(0, 255, 0));
-//		}
-//
-//		String filename = "ouput.png";
-//		System.out.println(String.format("Writing %s", filename));
-//		Imgcodecs.imwrite(filename, image);
-//	}
-	
+
+	// @FXML
+	// public void detectaFaces(){
+	// f = selectImage();
+	// String path = f.getAbsolutePath();
+	//
+	// System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	//
+	// CascadeClassifier faceDetector = new
+	// CascadeClassifier("haarcascade_frontalface_alt.xml");
+	// Mat image = Imgcodecs.imread("rostos.jpg");
+	//
+	// MatOfRect faceDetections = new MatOfRect();
+	// faceDetector.detectMultiScale(image, faceDetections);
+	//
+	// System.out.println(String.format("Detected %s faces",
+	// faceDetections.toArray().length));
+	//
+	// for (Rect rect : faceDetections.toArray()) {
+	// Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x +
+	// rect.width, rect.y + rect.height),
+	// new Scalar(0, 255, 0));
+	// }
+	//
+	// String filename = "ouput.png";
+	// System.out.println(String.format("Writing %s", filename));
+	// Imgcodecs.imwrite(filename, image);
+	// }
+
 	@FXML
-	public void erosaoDilatacao(){
+	public void erosaoDilatacao() {
+    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat matImgDst = new Mat();
 		int kernelSize = Integer.parseInt(krnlsz.getText());
 		String tipo = tp.getText();
-		 matImgSrc = OpenCVUtils.image2Mat(img_1);
-		 Mat element = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(2 * kernelSize + 1, 2 * kernelSize + 1),
-	                new Point(kernelSize, kernelSize));
-	        if (tipo.equals("erosão")) {
-	            Imgproc.erode(matImgSrc, matImgDst, element);
-	        } else {
-	            Imgproc.dilate(matImgSrc, matImgDst, element);
-	        }
-//	        java.awt.Image imagem = HighGui.toBufferedImage(matImgDst);
-	        Image imgTest = OpenCVUtils.matrixToImage(matImgDst);
-	        img_3 = imgTest;
-	        updateImage3();
-	        
+		matImgSrc = OpenCVUtils.imageToMat(img_1);
+		Mat element = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,
+				new Size(2 * kernelSize + 1, 2 * kernelSize + 1), new Point(kernelSize, kernelSize));
+		if (tipo.equals("erosão")) {
+			Imgproc.erode(matImgSrc, matImgDst, element);
+		} else {
+			Imgproc.dilate(matImgSrc, matImgDst, element);
+		}
+		Image imgTest = OpenCVUtils.matrixToImage(matImgDst);
+		img_3 = imgTest;
+		updateImage3();
+
 	}
-	
-	
+
 	@FXML
-	public void negativa(){
+	public void negativa() {
 		img_3 = Pdi.negativa(img_1);
 		updateImage3();
 	}
-	
 
 	@FXML
-	public void limiar(){
+	public void limiar() {
 		double value = slider.getValue();
 		value = value / 255;
 		img_3 = Pdi.limiar(img_1, value);
 		updateImage3();
 	}
-		
+
 	@FXML
-	public void Adiciona(){
+	public void Adiciona() {
 		img_3 = Pdi.adicao(img_1, img_2, Double.parseDouble(pctImg1.getText()), Double.parseDouble(pctImg2.getText()));
 		updateImage3();
 	}
-	
+
 	@FXML
-	public void subtrai(){
+	public void subtrai() {
 		img_3 = Pdi.subtrai(img_1, img_2, Integer.parseInt(pctImg1.getText()), Integer.parseInt(pctImg2.getText()));
 		updateImage3();
 	}
-	
+
 	@FXML
-    public void openImage1(){
-		f = selectImage();	
-		if( f != null){
+	public void openImage1() {
+		f = selectImage();
+		if (f != null) {
 			img_1 = new Image(f.toURI().toString());
 			image_view_1.setImage(img_1);
 			image_view_1.setFitWidth(img_1.getWidth());
 			image_view_1.setFitHeight(img_1.getHeight());
 		}
-    }
-	
-	@FXML 
-	public void openImage2(){
-		f = selectImage();	
-		if( f != null){
+	}
+
+	@FXML
+	public void openImage2() {
+		f = selectImage();
+		if (f != null) {
 			img_2 = new Image(f.toURI().toString());
 			image_view_2.setImage(img_2);
 			image_view_2.setFitWidth(img_2.getWidth());
@@ -186,106 +202,102 @@ public class Controller {
 	}
 
 	@FXML
-	private void updateImage3(){
+	private void updateImage3() {
 		image_view_3.setImage(img_3);
 		image_view_3.setFitWidth(img_3.getWidth());
 		image_view_3.setFitHeight(img_3.getHeight());
 	}
-	
-	
+
 	@FXML
-	public void greyScaleMedian(){
-		img_3 = Pdi.greyScale(img_1, 0,0,0);
+	public void greyScaleMedian() {
+		img_3 = Pdi.greyScale(img_1, 0, 0, 0);
 		updateImage3();
 	}
-	
+
 	@FXML
-	public void greyScaleP(){
+	public void greyScaleP() {
 		int rv = Integer.parseInt(r_percentage.getText());
 		int gv = Integer.parseInt(g_percentage.getText());
 		int bv = Integer.parseInt(b_percentage.getText());
-		
+
 		img_3 = Pdi.greyScale(img_1, rv, gv, bv);
 		updateImage3();
 	}
-	
-	
-	public void save(){
-		if(img_3 != null){
+
+	public void save() {
+		if (img_3 != null) {
 			FileChooser fc = new FileChooser();
 			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image", "*.png*"));
 			fc.setInitialDirectory(new File("C://users/mathe/Desktop/Processamento-Digital-de-Imagens/src/utils/imgs"));
 			File f = fc.showSaveDialog(null);
-			if(f != null){
+			if (f != null) {
 				BufferedImage bImg = SwingFXUtils.fromFXImage(img_3, null);
-				try{
+				try {
 					ImageIO.write(bImg, "PNG", f);
-				}catch(Exception e){
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
 	}
-	
+
 	@FXML
-	public void abreHistograma(ActionEvent event){
-		try{
+	public void abreHistograma(ActionEvent event) {
+		try {
 			Stage stage = new Stage();
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("Histograma.fxml"));
 			Parent root = loader.load();
 			stage.setScene(new Scene(root));
 			stage.setTitle("Histograma");
 			stage.initModality(Modality.WINDOW_MODAL);
-			stage.initOwner(((Node)event.getSource()).getScene().getWindow());
+			stage.initOwner(((Node) event.getSource()).getScene().getWindow());
 			stage.show();
-			
-			HistogramaController cntrllr = (HistogramaController)loader.getController();
-			
-			if(img_1!=null)
+
+			HistogramaController cntrllr = (HistogramaController) loader.getController();
+
+			if (img_1 != null)
 				Pdi.getGrafico(img_1, cntrllr.graph1);
-				img_3 = Pdi.equaliza(img_1);
-				updateImage3();
-				Pdi.getGrafico(img_3, cntrllr.graph3);
-//			if(img_2!=null)
-//				Pdi.getGrafico(img_2, cntrllr.graph2);
-//			if(img_3!=null)
-//				Pdi.getGrafico(img_3, cntrllr.graph3);
-			
-		}catch(Exception e){
+			img_3 = Pdi.equaliza(img_1);
+			updateImage3();
+			Pdi.getGrafico(img_3, cntrllr.graph3);
+			// if(img_2!=null)
+			// Pdi.getGrafico(img_2, cntrllr.graph2);
+			// if(img_3!=null)
+			// Pdi.getGrafico(img_3, cntrllr.graph3);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	private void showMessage(String title, String header, String msg, AlertType type){
+
+	private void showMessage(String title, String header, String msg, AlertType type) {
 		Alert alert = new Alert(type);
 		alert.setTitle(title);
 		alert.setHeaderText(header);
 		alert.setContentText(msg);
 		alert.showAndWait();
 	}
-	
-	
+
 	@FXML
-	public void clear(){
-		
+	public void clear() {
+
 	}
-	
+
 	@FXML
-	public void rasterImg(MouseEvent evt){
+	public void rasterImg(MouseEvent evt) {
 		ImageView iv = (ImageView) evt.getTarget();
-		if(iv.getImage() != null){
+		if (iv.getImage() != null) {
 			getRgb(iv.getImage(), (int) evt.getX(), (int) evt.getY());
 		}
 	}
-	
-	public void getRgb(Image img, int x, int y){
-		try{
-		Color cor = img.getPixelReader().getColor(x, y);
-		label_R.setText("R: " + (int) (cor.getRed()*255));
-		label_G.setText("G: " + (int) (cor.getGreen()*255));
-		label_B.setText("B: " + (int) (cor.getBlue()*255));
-		}catch(Exception e){
+
+	public void getRgb(Image img, int x, int y) {
+		try {
+			Color cor = img.getPixelReader().getColor(x, y);
+			label_R.setText("R: " + (int) (cor.getRed() * 255));
+			label_G.setText("G: " + (int) (cor.getGreen() * 255));
+			label_B.setText("B: " + (int) (cor.getBlue() * 255));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/utils/OpenCVUtils.java
+++ src/utils/OpenCVUtils.java
@@ -6,11 +6,15 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.ByteArrayInputStream;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.nio.file.Paths;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritablePixelFormat;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
@@ -52,6 +56,21 @@ public class OpenCVUtils {
 
         return bufferedImage2Mat( bImage);
 
+    }
+    
+    public static Mat imageToMat(Image image) {
+    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        byte[] buffer = new byte[width * height * 4];
+
+        PixelReader reader = image.getPixelReader();
+        WritablePixelFormat<ByteBuffer> format = WritablePixelFormat.getByteBgraInstance();
+        reader.getPixels(0, 0, width, height, format, buffer, 0, width * 4);
+
+        Mat mat = new Mat(height, width, CvType.CV_8UC4);
+        mat.put(0, 0, buffer);
+        return mat;
     }
 
     // http://www.codeproject.com/Tips/752511/How-to-Convert-Mat-to-BufferedImage-Vice-Versa
